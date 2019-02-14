35d0d39b6b4414e28b770afe97be5c5907413978
legendarmy <624125502@qq.com>
2019-02-14 12:56:03 +0800

    222

--- jedis_client/src/main/java/jedis_client/App.java
+++ jedis_client/src/main/java/jedis_client/App.java
@@ -11,9 +11,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
-        jedis.set("hello1", "world1");
-        String value = jedis.get("hello1");
-        System.out.println(value);
+        // Jedis jedis = new Jedis("127.0.0.1", 6379);
+        // jedis.set("hello1", "world1");
+        // String value = jedis.get("hello1");
+        // System.out.println(value);
+        App app = new App();
+        app.jedisBasic();
+    }
+
+    public void jedisBasic()
+    {
+        Jedis jedis = null;
+        try {
+            jedis = new Jedis("127.0.0.1", 6379);
+            jedis.get("hello1");
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println(e.getMessage());
+        } finally {
+            if (jedis != null)
+                jedis.close();
+        }
     }
 }
